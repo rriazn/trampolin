43ca5b0f1b18fad0f00d01e52a85dd2758d39b7a
rriazn <rnriazanov@gmail.com>
2026-06-17 11:24:29 +0200

    Fix integration tests

--- playwright.integration.config.js
+++ playwright.integration.config.js
@@ -5,6 +5,13 @@ module.exports = defineConfig({
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
     trace: 'on-first-retry',
+    launchOptions: {
+      args: [
+        '--disable-features=AutoupgradeHTTP',
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ],
+    },
   },
   workers: 1,
 });
